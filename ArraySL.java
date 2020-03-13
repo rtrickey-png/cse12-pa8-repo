1ef4181826893d138ed98540d562941a77af839d
rtrickey-png <60756622+rtrickey-png@users.noreply.github.com>
2020-03-12 19:41:08 -0700

    Add files via upload

--- ArraySL.java
+++ ArraySL.java
@@ -30,20 +30,91 @@ public class ArraySL implements StringList {
 	
 	public void chooseAll(StringChooser sc) { 
 		int count = 0;
+		int index1 = -1, index2 = -1, index3 = -1;
 		for(int i = 0; i < size; ++i) {
 			if(sc.chooseElement(elements[i]))
+				{
 				count++;
+				if(index1 == -1)
+					{
+					index1 = i;
+					continue;
+					}
+				if(index2 == -1) {
+					index2 = i;
+					continue;
+				}
+				if(index3 == -1) {
+					index3 = i;
+					continue;
+				}
+				
+				}
 		}
+		if(count != 0) {
 		String[] newElements = new String[count];
 		int index = 0;
-		for(int i = 0; i < size; ++i) {
-			if(sc.chooseElement(elements[i])) {
-				newElements[index] = elements[i];
-				index++;
+		if(index1 > -1) {
+			newElements[index] = elements[index1];
+			index++;
+		}
+		if(count == 1) {
+			elements = newElements;
+			size = count;
+			return;
+		}
+		if(index2 > -1) {
+			newElements[index] = elements[index2];
+			index++;
+		}
+		if(count == 2) {
+			elements = newElements;
+			size = count;
+			return;
+		}
+		
+		if(index3 > -1) {
+			newElements[index] = elements[index3];
+			index++;
+		}
+		if(count == 3) {
+			elements = newElements;
+			size = count;
+			return;
+		}
+		if(index1 > -1 && index2 > -1 && index3 > -1) {
+			for(int i = index3+1; i < size; ++i) {
+				if(sc.chooseElement(elements[i])) {
+					newElements[index] = elements[i];
+					index++;
+				}
 			}
 		}
+		else if(index1 > -1 && index2 > -1) {
+			for(int i = index2+1; i < size; ++i) {
+				if(sc.chooseElement(elements[i])) {
+					newElements[index] = elements[i];
+					index++;
+				}
+			}
+		}
+		else /*if(index1 > -1) */{
+			for(int i = index1+1; i < size; ++i) {
+				if(sc.chooseElement(elements[i])) {
+					newElements[index] = elements[i];
+					index++;
+				}
+			}
+		}
+		
 		elements = newElements;
 		size = count;
+		}
+		else {
+			elements = new String[0];
+			size = 0;
+		}
+		
 	}
 	
 	public boolean isEmpty() {
